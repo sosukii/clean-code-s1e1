937bc2174d144cb3e99d14e801f9336d390ff1c3
Svetlana Trukhina <sweta4ca@mail.ru>
2023-04-23 14:55:49 +0700

    fix(extended-1.2): html - add alt to img, js - set alt for icon when create it

--- app.js
+++ app.js
@@ -47,6 +47,7 @@ var createNewTaskElement=function(taskString){
     deleteButton.classList.add('task-btn-delete', 'btn')
     deleteButtonImg.src='./remove.svg';
     deleteButtonImg.classList.add('remove-img')
+    deleteButtonImg.setAttribute('alt', 'remove icon')
     deleteButton.appendChild(deleteButtonImg);
 
 
